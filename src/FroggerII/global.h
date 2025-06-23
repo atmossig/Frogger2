f2b44194bc02150994a5067854c83cb590774471
grant <80866912+atmossig@users.noreply.github.com>
2025-06-23 01:45:43 -0500

    Global config!

---
+++ src/FroggerII/global.h
@@ -0,0 +1,21 @@
+﻿/* 
+*	Global configuration file
+*	
+*	06-23-2025  atmossig	
+*/
+
+#ifndef _GLOBAL_H
+#define _GLOBAL_H
+
+// Platform definitions
+#if defined(__WIN32) || defined(_WIN64)
+#define PC_VERSION
+#elif defined(__linux__)
+#define LINUX_VERSION
+#elif defined(__APPLE__)
+#define MAC_VERSION
+#elif defined(__ANDROID__)
+#define ANDROID_VERSION
+#endif
+
+#endif // _GLOBAL_H
